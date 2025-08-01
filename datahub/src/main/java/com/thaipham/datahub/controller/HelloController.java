c5eda93b8888b06f4e99e0a9710706a581212e3e
Thai Pham <hp.thai3@gmail.com>
2025-08-01 09:16:12 -0500

    feat: add basic frontend served via static index.html
    - Created index.html with basic UI for testing API responses
    - Moved index.html to src/main/resources/static for Spring Boot static serving
    - Confirmed frontend accessible at /index.html and fetches data from backend

--- datahub/src/main/java/com/thaipham/datahub/controller/HelloController.java
+++ datahub/src/main/java/com/thaipham/datahub/controller/HelloController.java
@@ -1,14 +1,19 @@
 package com.thaipham.datahub.controller;
 
-import org.springframework.web.bind.annotation.*;
-
 import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/api/hello")
     public Map<String, String> hello() {
         return Map.of("message", "Hello from Spring Boot!");
     }
